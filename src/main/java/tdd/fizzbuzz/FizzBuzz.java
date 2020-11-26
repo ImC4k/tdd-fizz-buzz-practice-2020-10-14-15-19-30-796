ed91e5c662feda9434aed86936231dbc80bce345
Calvin Chu <calvin.chu@oocl.com>
2020-11-26 14:37:09 +0800

    [fizz-buzz][Calvin] refactor: special strings

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,17 +5,20 @@ public class FizzBuzz {
     public static final int SPECIAL_NUMBER_FOR_FIZZ = 3;
     public static final int SPECIAL_NUMBER_FOR_BUZZ = 5;
     public static final int SPECIAL_NUMBER_FOR_WHIZZ = 7;
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
 
     public String countOff(int order) {
         String shoutMessage = "";
         if (order % SPECIAL_NUMBER_FOR_FIZZ == 0) {
-            shoutMessage += "Fizz";
+            shoutMessage += FIZZ;
         }
         if (order % SPECIAL_NUMBER_FOR_BUZZ == 0) {
-            shoutMessage += "Buzz";
+            shoutMessage += BUZZ;
         }
         if (order % SPECIAL_NUMBER_FOR_WHIZZ == 0) {
-            shoutMessage += "Whizz";
+            shoutMessage += WHIZZ;
         }
         return shoutMessage.isEmpty()? String.valueOf(order) : shoutMessage;
     }
