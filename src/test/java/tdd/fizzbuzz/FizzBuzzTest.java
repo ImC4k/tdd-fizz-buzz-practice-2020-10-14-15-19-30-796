2509162106e8df9bd4d376f8434aadff324c410f
Calvin Chu <calvin.chu@oocl.com>
2020-11-26 13:55:58 +0800

    [fizz-buzz][Calvin] test: implemented test for normal case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_string_of_number_when_count_off_given_non_special_number() {
+        // given
+        int order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String actual = fizzBuzz.countOff(order);
+
+        // then
+        assertEquals(String.valueOf(order), actual);
+    }
 }
