9222d6bdc775e63548963b59458e9842b884f8ee
Calvin Chu <calvin.chu@oocl.com>
2020-11-26 14:12:17 +0800

    [fizz-buzz][Calvin] refactor: constant for buzz special number

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,12 +3,13 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final int SPECIAL_NUMBER_FOR_FIZZ = 3;
+    public static final int SPECIAL_NUMBER_FOR_BUZZ = 5;
 
     public String countOff(int order) {
         if (order % SPECIAL_NUMBER_FOR_FIZZ == 0) {
             return "Fizz";
         }
-        else if (order % 5 == 0) {
+        else if (order % SPECIAL_NUMBER_FOR_BUZZ == 0) {
             return "Buzz";
         }
         return String.valueOf(order);
