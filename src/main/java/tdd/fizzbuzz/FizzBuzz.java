0f95bdd40bd5bb6196de50a9b85314124994a381
Calvin Chu <calvin.chu@oocl.com>
2020-11-26 14:06:14 +0800

    [fizz-buzz][Calvin] refactor: multiple of first special number case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final int SPECIAL_NUMBER_FOR_FIZZ = 3;
+
     public String countOff(int order) {
-        if (order % 3 == 0) {
+        if (order % SPECIAL_NUMBER_FOR_FIZZ == 0) {
             return "Fizz";
         }
         return String.valueOf(order);
